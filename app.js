08f04cf9abd5f0965b3fd2de1f3eb4cbb4427833
Viktor <viktor.hidirov@yandex.ru>
2023-07-14 22:27:06 +0700

    -

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const helmet = require('helmet');
 const { route } = require('./routes');
 const { PORT, MONGO_URL } = require('./utils/config');
 
 const app = express();
-// app.use(cors());
+
+app.use((req, res) => {
+  res.header('Access-Control-Allow-Origin', '*');
+});
+
+app.use(cors());
 
 app.use(helmet());
 
