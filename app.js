b1dc13b7d9fef7043bfb00f575c4486a47b51f4d
Viktor <viktor.hidirov@yandex.ru>
2023-07-14 22:23:16 +0700

    времено убрал корс

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ require('dotenv').config();
 
 const express = require('express');
 const mongoose = require('mongoose');
-const cors = require('cors');
+// const cors = require('cors');
 const helmet = require('helmet');
 const { route } = require('./routes');
 const { PORT, MONGO_URL } = require('./utils/config');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 
 app.use(helmet());
 
